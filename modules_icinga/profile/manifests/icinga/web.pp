65ecf04dc79726c4152c88b6140d948a72c59889
Walter Heck <walterheck@olindata.com>
2015-10-24 05:24:09 +0200

    Add apache management to icinga::web profile

--- modules_icinga/profile/manifests/icinga/web.pp
+++ modules_icinga/profile/manifests/icinga/web.pp
@@ -1,7 +1,11 @@
 class profile::icinga::web {
   class { 'epel': } ->
+  class { 'apache': 
+    default_vhost => false,
+  } ->
   class { 'icingaweb2':
     manage_repo    => true,
     install_method => 'package',
+    manage_apache_vhost => true,
   }
 }
