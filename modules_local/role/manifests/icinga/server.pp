fadbf8096e2102ceead8c54b1997ad78bd885421
Walter Heck <walterheck@olindata.com>
2015-11-03 15:10:24 +0100

    Move the icinga web profile to it's own vm

--- modules_local/role/manifests/icinga/server.pp
+++ modules_local/role/manifests/icinga/server.pp
@@ -1,5 +1,4 @@
 class role::icinga::server {
   include profile::base
   include profile::icinga::server
-  include profile::icinga::web
 }
--- modules_local/role/manifests/icinga/web.pp
+++ modules_local/role/manifests/icinga/web.pp
@@ -1,3 +1,4 @@
 class role::icinga::web {
   include profile::base
+  include profile::icinga::web  
 }
