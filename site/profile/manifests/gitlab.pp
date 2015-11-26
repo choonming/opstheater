83520eb847e34d9dd7532ae3b245f5099ee7143b
Walter Heck <walterheck@olindata.com>
2015-11-26 12:50:02 +0100

    Enable mattermost and gitlab ci
    Trying to enable mattermost and gitlab ci by setting up the etxernal urls

--- site/profile/manifests/gitlab.pp
+++ site/profile/manifests/gitlab.pp
@@ -1,5 +1,7 @@
 class profile::gitlab {
   class { '::gitlab':
-    external_url => 'http://gitlab.olindata.vm',
+    external_url            => 'http://gitlab.olindata.vm',
+    ci_external_url         => 'http://ci.olindata.vm',
+    mattermost_external_url => 'http://chat.olindata.vm'
   }
 }
