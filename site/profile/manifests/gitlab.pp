3dbda78a1746b6a3044fcb1c9b9f37079e246d35
Walter Heck <walterheck@olindata.com>
2015-11-29 17:39:58 +0100

    Make small fixes to gitlab profile
    As it turns out we need to create the gitlab-ci user and the /var/opt/gitlab/nginx/conf directory before running the omnibus installer.
    Also fixed a small problem with the host{} resource that gets created on the gitlab node

--- site/profile/manifests/gitlab.pp
+++ site/profile/manifests/gitlab.pp
@@ -1,4 +1,18 @@
 class profile::gitlab {
+
+  # NOTE: it shouldn't be needed to define the user and file resources here, this should be 
+  # fixed in the omnibus installer
+  user { 'gitlab-ci':
+    comment => 'Gitlab CI user',
+    home    => '/var/opt/gitlab/gitlab-ci',
+    ensure  => present,
+    shell  => '/bin/false',
+  } ->
+
+  file { '/var/opt/gitlab/nginx/conf':
+    ensure => directory,
+  } ->
+
   class { '::gitlab':
     external_url            => 'http://gitlab.olindata.vm',
     ci_external_url         => 'http://ci.olindata.vm',
@@ -7,7 +21,7 @@ class profile::gitlab {
 
   host { ['gitlab.olindata.vm', 'ci.olindata.vm', 'chat.olindata.vm']:
     ensure => present,
-    ip     => $facts['networking']['interfaces']['enp0s8'],
+    ip     => $facts['networking']['interfaces']['enp0s8']['ip'],
   }
 
 }
