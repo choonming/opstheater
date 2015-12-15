0cb2da17f318d1add1221ca753a278a11ca86037
Walter Heck <walterheck@olindata.com>
2015-12-15 19:30:47 +0100

    Adds a first gitlab user through the new defined type in the gitlab module
    this one is just a test, it will be replaced by real users later

--- site/role/manifests/gitlab/server.pp
+++ site/role/manifests/gitlab/server.pp
@@ -2,4 +2,11 @@ class role::gitlab::server {
   include profile::base  
 
   include profile::gitlab
+
+  gitlab::user { 'walter-test':
+    username => 'walterheck',
+    email    => 'walterheck@olindata.com',
+    password => 'alkgrcfnal',
+    fullname => 'Walter Heck',
+  }
 }
