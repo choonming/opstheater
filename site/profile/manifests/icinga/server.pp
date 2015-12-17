a65ba34ca5aed5386ec1de0fc8ed95e9157df0ec
Choon Ming Goh <choon.ming@choonming.org>
2015-12-17 23:24:46 +0100

    initial commit for full working icinga2 and icingaweb2 setup

--- site/profile/manifests/icinga/server.pp
+++ site/profile/manifests/icinga/server.pp
@@ -1,36 +1,64 @@
 class profile::icinga::server {
 
-  class { 'postgresql::globals':
-    manage_package_repo => true,
-    version             => '9.3',
-  }->
-  
-  class { 'postgresql::server':  listen_addresses => '*', }->
-  
-  postgresql::server::db { 'icinga2_data':
-    user     => 'icinga2',
-    password => postgresql_password('icinga2', 'password'),
-  }->
-  postgresql::server::db { 'icinga2_web':
-    user     => 'icingaweb',
-    password => postgresql_password('icingaweb', 'password'),
-  }->
+  class { '::mysql::server':
+    override_options  => { 
+      'mysqld' => {
+        'bind-address' => '0.0.0.0',
+      }
+    },
+  }
+
+  mysql::db { 'icinga2_data':
+    user      => 'icinga2',
+    password  => 'password',
+    host      => '10.20.%',
+    grant     => ['ALL'],
+    before    => Class['icinga2::server'],
+  } 
+
+  mysql::db { 'icinga2_data_localhost':
+    dbname    => 'icinga2_data',
+    user      => 'icinga2',
+    password  => 'password',
+    host      => 'localhost',
+    grant     => ['ALL'],
+    before    => Class['icinga2::server'],
+  } 
+
+  mysql::db { 'icinga2_web':
+    user      => 'icinga2_web',
+    password  => 'password',
+    host      => '10.20.%',
+    grant     => ['ALL'],
+    before    => Class['icinga2::server'],
+  } 
+
+  mysql::db { 'icinga2_web_localhost':
+    dbname    => 'icinga2_web',
+    user      => 'icinga2_web',
+    password  => 'password',
+    host      => 'localhost',
+    grant     => ['ALL'],
+    before    => Class['icinga2::server'],
+  } 
 
   class { 'icinga2::server':
-    server_db_type => 'pgsql',
+    server_db_type => 'mysql',
     db_host => 'localhost',
-    db_port => '5432',
+    db_port => '3306',
     db_name => 'icinga2_data',
     db_user => 'icinga2',
     db_password => 'password',
   }->
-  icinga2::object::idopgsqlconnection { 'postgres_connection':
+
+  icinga2::object::idomysqlconnection { 'mysql_connection':
      target_dir => '/etc/icinga2/features-enabled',
-     target_file_name => 'ido-pgsql.conf',
+     target_file_name => 'ido-mysql.conf',
      host             => '127.0.0.1',
-     port             => 5432,
+     port             => '3306',
      user             => 'icinga2',
      password         => 'password',
      database         => 'icinga2_data',
   }
+
 }
