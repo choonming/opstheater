26e704848ee728dffaf695a1565a4fe8efa67448
Choon Ming Goh <choon.ming@choonming.org>
2015-12-21 21:15:55 +0100

    undo commit 97b7a as it still needs to support multi server roles

--- site/profile/manifests/icinga/web.pp
+++ site/profile/manifests/icinga/web.pp
@@ -32,14 +32,14 @@ class profile::icinga::web {
     manage_apache_vhost => true,
     apache_vhost_name => 'icinga.olindata.com',
     ido_db  => 'mysql',
-    ido_db_host => 'localhost',
+    ido_db_host => 'icinga2',
     ido_db_name => 'icinga2_data',
     ido_db_user => 'icinga2',
     ido_db_pass => 'password',
     ido_db_port => '3306',
     web_db      => 'mysql',
     web_db_name => 'icinga2_web',
-    web_db_host => 'localhost',
+    web_db_host => 'icinga2',
     web_db_user => 'icinga2_web',
     web_db_pass => 'password',
     web_db_port => '3306',
@@ -47,8 +47,8 @@ class profile::icinga::web {
 
   exec { 'populate-icinga2_web-mysql-db':
     path  => '/bin:/usr/bin:/sbin:/usr/sbin',
-    unless    => 'mysql -h localhost -uicinga2_web -ppassword icinga2_web -e "SELECT * FROM icingaweb_user;" &> /dev/null',
-    command   => 'mysql -h localhost -uicinga2_web -ppassword icinga2_web < /usr/share/doc/icingaweb2/schema/mysql.schema.sql; mysql -h localhost -uicinga2_web -ppassword icinga2_web -e "INSERT INTO icingaweb_user (name, active, password_hash) VALUES (\'icingaadmin\', 1, \'\$1\$iQSrnmO9\$T3NVTu0zBkfuim4lWNRmH.\');"',
+    unless    => 'mysql -h icinga2 -uicinga2_web -ppassword icinga2_web -e "SELECT * FROM icingaweb_user;" &> /dev/null',
+    command   => 'mysql -h icinga2 -uicinga2_web -ppassword icinga2_web < /usr/share/doc/icingaweb2/schema/mysql.schema.sql; mysql -h icinga2 -uicinga2_web -ppassword icinga2_web -e "INSERT INTO icingaweb_user (name, active, password_hash) VALUES (\'icingaadmin\', 1, \'\$1\$iQSrnmO9\$T3NVTu0zBkfuim4lWNRmH.\');"',
     require   => [ Class['::mysql::client'], Package['icingaweb2'] ],
   } ->
 
