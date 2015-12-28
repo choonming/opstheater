a5401e7099d51c362c79358385df9794b0eae73e
Walter Heck <walterheck@olindata.com>
2015-12-28 14:18:25 +0100

    Kibana doesn't like port 80 very much, so we'll switch back to 5601 for now.

--- site/profile/manifests/kibana.pp
+++ site/profile/manifests/kibana.pp
@@ -3,7 +3,6 @@ class profile::kibana {
   # use the standard kibana4 class, the parameters will be loaded from hiera
   class { '::kibana4':
     elasticsearch_url => 'http://elasticsearch.olindata.vm:9200',
-    server_port       => 80,
   } 
 
 }
