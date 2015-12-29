49ec961415ec8290c58ed46d9a8754bc58bad8b0
Walter Heck <walterheck@olindata.com>
2015-12-29 17:13:44 +0100

    Implemented more global opstheater config values
    Over the next couple of weeks we need to have every value that customers are allowed to change in the opstheater scope. This commit is a first attempt at finding and replacing some of these values

--- site/profile/manifests/icinga/server.pp
+++ site/profile/manifests/icinga/server.pp
@@ -1,5 +1,7 @@
 class profile::icinga::server {
 
+  $icinga2_db_fqdn = hiera('opstheater::icinga::mysql_fqdn')
+
   class { '::mysql::server':
     override_options  => { 
       'mysqld' => {
@@ -25,26 +27,9 @@ class profile::icinga::server {
     before    => Class['icinga2::server'],
   } 
 
-  mysql::db { 'icinga2_web':
-    user      => 'icinga2_web',
-    password  => 'password',
-    host      => '10.20.%',
-    grant     => ['ALL'],
-    before    => Class['icinga2::server'],
-  } 
-
-  mysql::db { 'icinga2_web_localhost':
-    dbname    => 'icinga2_web',
-    user      => 'icinga2_web',
-    password  => 'password',
-    host      => 'localhost',
-    grant     => ['ALL'],
-    before    => Class['icinga2::server'],
-  } 
-
   class { 'icinga2::server':
     server_db_type => 'mysql',
-    db_host => 'localhost',
+    db_host => $icinga2_db_fqdn,
     db_port => '3306',
     db_name => 'icinga2_data',
     db_user => 'icinga2',
@@ -54,7 +39,7 @@ class profile::icinga::server {
   icinga2::object::idomysqlconnection { 'mysql_connection':
      target_dir => '/etc/icinga2/features-enabled',
      target_file_name => 'ido-mysql.conf',
-     host             => '127.0.0.1',
+     host             => $icinga2_db_fqdn,
      port             => '3306',
      user             => 'icinga2',
      password         => 'password',
--- site/profile/manifests/icinga/web.pp
+++ site/profile/manifests/icinga/web.pp
@@ -1,5 +1,8 @@
 class profile::icinga::web {
 
+  $icinga2_web_fqdn = hiera('opstheater::icingaweb::fqdn')
+  $icinga2_db_fqdn = hiera('opstheater::icingaweb::mysql_fqdn')
+
   if ! defined (Class['epel']) {
     class { 'epel': }
   }
@@ -30,25 +33,43 @@ class profile::icinga::web {
     manage_repo    => true,
     install_method => 'package',
     manage_apache_vhost => true,
-    apache_vhost_name => 'icinga2.olindata.vm',
+    apache_vhost_name => $icinga2_web_fqdn,
     ido_db  => 'mysql',
-    ido_db_host => 'icinga2.olindata.vm',
+    ido_db_host => $icinga2_db_fqdn,
     ido_db_name => 'icinga2_data',
     ido_db_user => 'icinga2',
     ido_db_pass => 'password',
     ido_db_port => '3306',
     web_db      => 'mysql',
     web_db_name => 'icinga2_web',
-    web_db_host => 'icinga2.olindata.vm',
+    web_db_host => $icinga2_db_fqdn,
     web_db_user => 'icinga2_web',
     web_db_pass => 'password',
     web_db_port => '3306',
   }
 
+  mysql::db { 'icinga2_web':
+    user      => 'icinga2_web',
+    password  => 'password',
+    host      => '10.20.%',
+    grant     => ['ALL'],
+    before    => Class['icinga2::server'],
+  } 
+
+  mysql::db { 'icinga2_web_localhost':
+    dbname    => 'icinga2_web',
+    user      => 'icinga2_web',
+    password  => 'password',
+    host      => 'localhost',
+    grant     => ['ALL'],
+    before    => Class['icinga2::server'],
+  } 
+
+
   exec { 'populate-icinga2_web-mysql-db':
     path  => '/bin:/usr/bin:/sbin:/usr/sbin',
-    unless    => 'mysql -h icinga2.olindata.vm -uicinga2_web -ppassword icinga2_web -e "SELECT * FROM icingaweb_user;" &> /dev/null',
-    command   => 'mysql -h icinga2.olindata.vm -uicinga2_web -ppassword icinga2_web < /usr/share/doc/icingaweb2/schema/mysql.schema.sql; mysql -h icinga2.olindata.vm -uicinga2_web -ppassword icinga2_web -e "INSERT INTO icingaweb_user (name, active, password_hash) VALUES (\'icingaadmin\', 1, \'\$1\$iQSrnmO9\$T3NVTu0zBkfuim4lWNRmH.\');"',
+    unless    => 'mysql -h ' + $icinga2_db_fqdn + '-uicinga2_web -ppassword icinga2_web -e "SELECT * FROM icingaweb_user;" &> /dev/null',
+    command   => 'mysql -h ' + $icinga2_db_fqdn + '-uicinga2_web -ppassword icinga2_web < /usr/share/doc/icingaweb2/schema/mysql.schema.sql; mysql -h ' + $icinga2_db_fqdn + ' -uicinga2_web -ppassword icinga2_web -e "INSERT INTO icingaweb_user (name, active, password_hash) VALUES (\'icingaadmin\', 1, \'\$1\$iQSrnmO9\$T3NVTu0zBkfuim4lWNRmH.\');"',
     require   => [ Class['::mysql::client'], Package['icingaweb2'] ],
   } ->
 
