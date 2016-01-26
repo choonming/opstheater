1a14ad25b5a7f9ea6ffe053a1750c1f98294b95d
Choon Ming Goh <choon.ming@choonming.org>
2016-01-26 20:22:50 +0100

    create cronjob for puppet agent runs

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -46,4 +46,13 @@ class profile::base {
     key   => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDBgMhKw6sIw50udszZbrtqO6svbrvjzjLGtURpfk52IWXqOib5tNC8HJtSUOyu4euY0QlpoLxGdnE5wDhfG8dWgFo9DAhONJ4alQkq7RxBLY3FF+W01PWVLV4lwdumCQ9XgrcXdG4MiVtWVyZdbabqltgNZILlEvtpCGpAiptEdSTek5eSV5wVF/mHyVMa/DLBTh2CNhxeG0//8vMurxFRgLGhg7u7bj3lqcPwMoiCaX9cnalPqQ3WryoobwfykHqvCLUhM0fpbkNAB+K5j3qK5SOlRxs9JMxcMjPNGqkcfQ3hCpnfhDwQLfjFliaG0yACxQTujg6jrqNdk4FW0FEx',
   }
 
+  $rand1 = fqdn_rand(30)
+  $rand2 = $rand1 + 30
+
+  cron { 'Puppet agent run':
+    command => '/opt/puppetlabs/bin/puppet agent --onetime --no-daemonize',
+    user    => 'root',
+    minute  => [ $rand1, $rand2],
+  }
+
 }
