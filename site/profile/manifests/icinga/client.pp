12e8938616b888d0fee872491f9c5f2317bf9927
Choon Ming Goh <choon.ming@choonming.org>
2016-02-03 19:19:03 +0100

    initial setup for icinga2 client setup

---
+++ site/profile/manifests/icinga/client.pp
@@ -0,0 +1,26 @@
+class profile::icinga::client {
+
+  $icinga2_web_fqdn = hiera('opstheater::icingaweb::fqdn')
+
+  include ::icinga2
+  include ::icinga2::feature::command
+  include ::icinga2::pki::puppet
+
+  class { '::icinga2::feature::api':
+    accept_commands => true,
+    accept_config   => true,
+    manage_zone     => false,
+  }
+
+  @@icinga::object::zone { $::fqdn:
+    endpoints => {
+      $::fqdn => {
+        host  => $::fqdn,
+      },
+    },
+    parent    => 'master',
+  }
+
+  Icinga2::Object::Zone <<| |>>
+
+}
