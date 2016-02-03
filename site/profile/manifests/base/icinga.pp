76b6d08aba058fd188ea18d0a6d88595a7ed0621
Choon Ming Goh <choon.ming@choonming.org>
2016-02-03 19:19:49 +0100

    * add apply rules to remote hosts * add remote endpoint option for remote check

--- site/profile/manifests/base/icinga.pp
+++ site/profile/manifests/base/icinga.pp
@@ -1,18 +1,32 @@
 class profile::base::icinga {
 
+  Icinga2::Object::Host {
+    display_name  => $::fqdn,
+    check_command   => 'cluster-zone',
+    target_dir      => "/etc/icinga2/objects/hosts",
+    target_file_name=> "${::fqdn}.conf",
+  }
+
+  Icinga2::Object::Apply_service {
+    assign_where => 'host.address && host.vars.remote == true && host.vars.remote_endpoint',
+    command_endpoint => 'host.vars.remote_endpoint',
+  }
+
   @@icinga2::object::host { $::fqdn:
-    display_name => $::fqdn,
-    ipv4_address => $::ipaddress_eth0,
-    groups => ['linux-servers'],
+    ipv4_address => $::ipaddress,
     vars => {
       os      => 'Linux',
       "disks" => {
         '"disks"'   => { },
         '"disk /"'  => { '"disk_partitions"' => '"/"' }
       },
+      remote   => true,
+      remote_endpoint => $::fqdn
     },
-    target_dir => '/etc/icinga2/objects/hosts',
-    target_file_name => "${fqdn}.conf"
+  }
+
+  @@icinga2::object::apply_service { "check_user":
+    check_command => 'users',
   }
 
 }
