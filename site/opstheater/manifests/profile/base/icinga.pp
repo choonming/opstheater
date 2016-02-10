f3181d3af993faaab3f22097d4aa259a9bd69334
Choon Ming Goh <choon.ming@choonming.org>
2016-02-10 18:54:26 +0800

    move all icinga apply rules to a separate icinga profile

--- site/opstheater/manifests/profile/base/icinga.pp
+++ site/opstheater/manifests/profile/base/icinga.pp
@@ -7,11 +7,6 @@ class opstheater::profile::base::icinga {
     target_file_name=> "${::fqdn}.conf",
   }
 
-  Icinga2::Object::Apply_service {
-    assign_where => 'host.address && host.vars.remote == true && host.vars.remote_endpoint',
-    command_endpoint => 'host.vars.remote_endpoint',
-  }
-
   @@icinga2::object::host { $::fqdn:
     ipv4_address => $::ipaddress,
     vars => {
@@ -23,33 +18,6 @@ class opstheater::profile::base::icinga {
       remote   => true,
       remote_endpoint => $::fqdn
     },
-  }
-
-  @@icinga2::object::apply_service { "user":
-    check_command => 'users',
-  }
-
-  @@icinga2::object::apply_service { "ssh":
-    check_command => 'ssh',
-  }
-
-  @@icinga2::object::apply_service { "load":
-    check_command => 'load',
-  }
-
-  @@icinga2::object::apply_service { "process":
-    check_command => 'procs',
-  }
-
-  @@icinga2::object::apply_service { "swap":
-    check_command => 'swap',
-  }
-
-  @@icinga2::object::apply_service { "disk":
-    check_command => 'disk',
-    display_name  => undef,
-    apply => 'for (disk => config in host.vars.disk)',
-    vars => 'config',
   }
 
 }
