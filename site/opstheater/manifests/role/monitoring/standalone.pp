8d93086e636a13b1adf1c8024d01b74580ff83f7
Choon Ming Goh <choon.ming@choonming.org>
2016-02-11 17:57:04 +0800

    add icinga checks profile to icinga role

--- site/opstheater/manifests/role/monitoring/standalone.pp
+++ site/opstheater/manifests/role/monitoring/standalone.pp
@@ -2,4 +2,5 @@ class opstheater::role::monitoring::standalone {
   include opstheater::profile::base
   include opstheater::profile::icinga::web
   include opstheater::profile::icinga::server
+  include opstheater::profile::icinga::checks
 }
