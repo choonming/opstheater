074d0fc54ebd0124bd51c9169705f439b15d16f9
Walter Heck <walterheck@olindata.com>
2016-02-22 23:45:09 +0100

    Move gitlab prospector back into the profile::filebeat::gitlab class

--- site/profile/manifests/filebeat/gitlab.pp
+++ site/profile/manifests/filebeat/gitlab.pp
@@ -1,13 +1,17 @@
 class profile::filebeat::gitlab{
 
- $gitlablogs = hiera_hash('profile::filebeat::gitlab::prospector', undef)
-
-  if $gitlablogs {
-     create_resources('filebeat::prospector', $gitlablogs)
-  } else {
-     notify {"No gitlab log path configured in filebeat for instance ${::fqdn}.":}
+  filebeat::prospector { 'gitlablogs':
+    paths => [
+      '/var/log/gitlab/nginx/gitlab_access.log',
+      '/var/log/gitlab/nginx/gitlab_error.log',
+      '/var/log/gitlab/nginx/gitlab_ci_access.log',
+      '/var/log/gitlab/nginx/gitlab_ci_error.log',
+      '/var/log/gitlab/gitlab-rails/production.log',
+      '/var/log/gitlab/unicorn/unicorn_stdout.log',
+      '/var/log/gitlab/unicorn/unicorn_stderr.log',
+    ],
+    log_type => 'gitlablogs-beat',
   }
-
 
 }
 
