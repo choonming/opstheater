86f8bcb4fe41021f1c654a71fd3e967c9d1b368c
Walter Heck <walterheck@olindata.com>
2016-02-23 00:43:37 +0100

    fix indentation in profile::gitlab

--- site/profile/manifests/logstash.pp
+++ site/profile/manifests/logstash.pp
@@ -1,47 +1,44 @@
 class profile::logstash{
 
-include profile::filebeat::logstash
+  include profile::filebeat::logstash
 
-$package_url = hiera('profile::logstash::package_url')
-$input_beats_port = hiera('profile::logstash::input_beats_port')
-$input_beats_type = hiera('profile::logstash::input_beats_type')
-$output_elasicsearch_hosts = hiera_array('profile::logstash::output_elasticsearch_hosts', undef)
-$output_codec = hiera('profile::logstash::output_codec')
-
-
-$plugins = hiera_hash('profile::logstash::plugins', undef)
+  $package_url               = hiera('profile::logstash::package_url')
+  $input_beats_port          = hiera('profile::logstash::input_beats_port')
+  $input_beats_type          = hiera('profile::logstash::input_beats_type')
+  $output_elasicsearch_hosts = hiera_array('profile::logstash::output_elasticsearch_hosts', undef)
+  $output_codec              = hiera('profile::logstash::output_codec')
 
 
-class { 'logstash':
-  package_url  => $package_url,
-  java_install => true,
-}
+  $plugins = hiera_hash('profile::logstash::plugins', undef)
 
 
-logstash::configfile { 'input_beats':
-  content => template('profile/input_beats.erb'),
-  order   => 1
-}
+  class { 'logstash':
+    package_url  => $package_url,
+    java_install => true,
+  }
 
-logstash::configfile { 'filter':
-  content => template('profile/filter.erb'),
-  order   => 2
-}
+  logstash::configfile { 'input_beats':
+    content => template('profile/input_beats.erb'),
+    order   => 1
+  }
 
-logstash::configfile { 'output_elasticsearch':
-  content => template('profile/output_elasticsearch.erb'),
-  order   => 3
-}
+  logstash::configfile { 'filter':
+    content => template('profile/filter.erb'),
+    order   => 2
+  }
 
-logstash::configfile { 'output_stdout':
-  content => template('profile/output_stdout.erb'),
-  order   => 4
-}
+  logstash::configfile { 'output_elasticsearch':
+    content => template('profile/output_elasticsearch.erb'),
+    order   => 3
+  }
 
+  logstash::configfile { 'output_stdout':
+    content => template('profile/output_stdout.erb'),
+    order   => 4
+  }
 
-if $plugins {
-
+  if $plugins {
     create_resources('logstash::plugin', $plugins)
-}
+  }
 
 }
