6c0a34f1e8e02ddd86547b7464804249056276af
Choon Ming Goh <choon.ming@choonming.org>
2016-02-24 22:20:09 +0530

    use pe_server_version fact instead of pe_version

--- site/opstheater/manifests/role/puppet/master.pp
+++ site/opstheater/manifests/role/puppet/master.pp
@@ -2,7 +2,7 @@ class opstheater::role::puppet::master {
   # Include our standard base packages
   include opstheater::profile::base
   
-  unless $::pe_version {
+  unless $::pe_server_version {
     # Add our secure (if specified) foreman proxy via Nginx
     include opstheater::profile::foremanproxy
   }
