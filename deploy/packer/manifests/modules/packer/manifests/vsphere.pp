a797a500256e3fba3ebd62f38c77181abcde7a0e
Walter Heck <walterheck@Walters-MacBook-Pro.local>
2016-08-03 10:15:01 +0200

    updated .gitignore so it doesn't ignore the packer module directory in the packer path. Also adding all files there that were previously missing.

---
+++ deploy/packer/manifests/modules/packer/manifests/vsphere.pp
@@ -0,0 +1,62 @@
+class packer::vsphere inherits packer::vsphere::params {
+
+  include packer::vsphere::repos
+  include packer::vsphere::networking
+  include packer::vsphere::fw
+
+  user { root:
+    ensure   => present,
+    password => "$qa_root_passwd"
+  }
+
+  case $::osfamily {
+    redhat: {
+      if $::operatingsystemrelease in ['24', '25'] {
+        Package {
+          provider => 'dnf',
+        }
+
+        file { '/etc/dhclient.conf':
+          owner   => 'root',
+          group   => 'root',
+          mode    => '0644',
+          source  => 'puppet:///modules/packer/vsphere/dhclient.conf',
+        }
+      }
+    }
+  }
+
+  package { $ruby_package:
+    ensure => present,
+  }
+
+  file { $bootstrap_file:
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    content => template("packer/vsphere/${bootstrap_file_source}"),
+  }
+
+  file { $startup_file:
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    content => template("packer/vsphere/${startup_file_source}"),
+  }
+
+  file { '/root/.ssh':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    ensure => directory,
+  }
+
+  file { '/root/.ssh/authorized_keys':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    source  => 'puppet:///modules/packer/vsphere/authorized_keys',
+    require => File[ '/root/.ssh' ]
+  }
+
+}
